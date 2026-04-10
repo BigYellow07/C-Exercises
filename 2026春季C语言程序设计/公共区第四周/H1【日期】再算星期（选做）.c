1d5bfb39be183a27e0df120b30c8f4690b6f925c
Andrew Huang <260916802+BigYellow07@users.noreply.github.com>
2026-04-10 18:21:33 +0800

    Add program to calculate the day of the week

---
+++ 2026春季C语言程序设计/公共区第四周/H1【日期】再算星期（选做）.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isLeapYear(int year);
+int days_of_the_month(int month, int year);
+
+int main(void)
+{
+    int year, month, day;
+    long TotalDays;
+    int y, m;
+    int result;
+    int max_day;
+
+    TotalDays = 0;
+    result = 0;
+    max_day = 0;
+
+    if(scanf("%d %d %d", &year, &month, &day) != 3)
+    {
+        return 0;
+    }
+
+    max_day = days_of_the_month(month, year);
+
+    if(month < 1 || month > 12)
+    {
+        printf("month is error.\n");
+        return 1;
+
+    }
+
+    if(day < 1 || day > max_day)
+    {
+        printf("day is error.\n");
+        return 1;
+        
+    }
+
+    for(y = 1900; y < year; y++)
+    {
+        TotalDays += isLeapYear(y) ? 366 : 365;
+    }
+
+    for(m = 1; m < month; m++)
+    {
+        TotalDays += days_of_the_month(m, year);
+    }
+
+    TotalDays += day;
+
+    result = TotalDays % 7;
+
+    printf("%d\n", result);
+
+    return 0;
+}
+
+bool isLeapYear(int year)
+{
+    int t;
+
+    t = (year % 4 == 0 && year % 100 != 0)
+     || (year  % 400 == 0);
+
+    return t;
+}
+
+int days_of_the_month(int month, int year)
+{
+    switch (month)
+    {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+        month = 31;
+        break;
+
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+        month = 30;
+        break;
+
+        case 2:
+        month = isLeapYear(year) ? 29 : 28;
+        break;
+    }
+
+    return month;
+}
