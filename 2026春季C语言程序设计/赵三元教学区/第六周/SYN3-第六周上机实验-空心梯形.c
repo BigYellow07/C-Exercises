088554ff60eb3f7d28b1a00a6cd7438b7c399b88
Andrew Huang <260916802+BigYellow07@users.noreply.github.com>
2026-04-15 15:59:43 +0800

    Add files via upload

---
+++ 2026春季C语言程序设计/赵三元教学区/第六周/SYN3-第六周上机实验-空心梯形.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int i, t, j, x, y;
+	
+	if(scanf("%d", &n) != 1)
+	{
+		return 1;
+	}
+	
+	if(n == 1)
+	{
+		printf("*\n");
+	}
+	else if(n == 2)
+	{
+		printf("****\n **\n");
+	}
+	else
+	{
+		for(x = 1; x <= n + 2*(n - 1); x ++)
+		{
+			printf("*");
+		}
+		
+		printf("\n");
+		
+		for(i = 2; i <= n - 1; i ++)
+		{
+			for(t = 1; t <= i - 1; t++)
+			{
+				printf(" ");
+			}
+			
+			printf("*");
+			
+			for(j = 1; j <= n + 2*(n - i - 1); j++)
+			{
+				printf(" ");
+			}
+			
+			printf("*");
+			
+			printf("\n");
+		}
+		
+		for(y = n; y <= n; y ++)
+		{
+			for(t = 1; t <= i - 1; t++)
+			{
+				printf(" ");
+			}
+			
+			for(x = 1; x <= n; x ++)
+			{
+				printf("*");
+			}
+			
+			printf("\n");
+		}
+	}
+	
+	return 0;
+}
