dbce7fb88239ee57d7e9907e04c501d2e9ab33a9
Andrew Huang <260916802+BigYellow07@users.noreply.github.com>
2026-04-20 21:53:09 +0800

    Create N-22. 听写.c

---
+++ 2026春季C语言程序设计/赵三元教学区/第七周/N-22. 听写.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int n;
+	int i;
+	
+	if(scanf("%d", &n) != 1)
+	{
+		return 1;
+	}
+	
+	int length = n + 1;
+	char *word = (char*)calloc(length, sizeof(char));
+	
+	for(i = 0; i <= n; i ++)
+	{
+		if(scanf("%c", &word[i]) != 1)
+		{
+			return 1;
+		}
+	}
+	
+	int order_s, order_t;
+	order_s = -1;
+	order_t = -1;
+	
+	for(i = 0; i <= n; i ++)
+	{
+		if(word[i] == 's')
+		{
+			order_s = i;
+			break;
+		}
+	}
+	
+	for(i = 0; i <= n; i ++)
+	{
+		if(word[i] == 't')
+		{
+			order_t = i;
+		}
+	}
+	
+	if(order_s < order_t && order_s != -1)
+	{
+		printf("good pronunciation\n");
+	}
+	else
+	{
+		printf("just like Eijvy\n");
+	}
+	
+	free(word);
+	
+	return 0; 
+}
